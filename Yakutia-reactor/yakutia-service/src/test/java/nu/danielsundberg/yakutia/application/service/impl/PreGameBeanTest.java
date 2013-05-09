c30167f411345a721dc74593dba5cc55c01e4748
freddejones <freddejones@gmail.com>
2013-05-10 00:41:32 +0200

    adding more unit tests for PreGameBean

--- Yakutia-reactor/yakutia-service/src/test/java/nu/danielsundberg/yakutia/application/service/impl/PreGameBeanTest.java
+++ Yakutia-reactor/yakutia-service/src/test/java/nu/danielsundberg/yakutia/application/service/impl/PreGameBeanTest.java
@@ -1,5 +1,6 @@
 package nu.danielsundberg.yakutia.application.service.impl;
 
+import junit.framework.Assert;
 import nu.danielsundberg.yakutia.entity.Player;
 import nu.danielsundberg.yakutia.application.service.exceptions.PlayerAlreadyExists;
 import nu.danielsundberg.yakutia.testcore.JpaTestCase;
@@ -8,22 +9,61 @@ import org.junit.Test;
 public class PreGameBeanTest extends JpaTestCase {
 
     private String playerName = "PLAYER";
+    private String email = "";
     private Player player;
 
-    @Test(expected = PlayerAlreadyExists.class)
-    public void playerNameAlreadyExistingPlayer() throws PlayerAlreadyExists{
-        createPlayer();
-
+    @Test
+    public void createANewPlayerTest() throws PlayerAlreadyExists {
         PreGameBean preGameBean = new PreGameBean();
         preGameBean.em = entityManager;
-        preGameBean.createNewPlayer("PLAYER","mail");
+        long pId =  preGameBean.createNewPlayer(playerName, "mail");
+        Assert.assertNotNull(pId);
+    }
 
+    @Test(expected = PlayerAlreadyExists.class)
+    public void playerNameAlreadyExist() throws PlayerAlreadyExists{
+        createPlayer();
+        PreGameBean preGameBean = new PreGameBean();
+        preGameBean.em = entityManager;
+        preGameBean.createNewPlayer(playerName, "mail");
+    }
+
+    @Test
+    public void playerExistCheckTrue() {
+        PreGameBean preGameBean = new PreGameBean();
+        preGameBean.em = entityManager;
+
+        // Given: one player with a email
+        email = "email";
+        createPlayer();
+
+        // When: check if player exists method is called
+        boolean doExist = preGameBean.playerExists(email);
+
+        // Then: return true since same email
+        Assert.assertTrue(doExist);
+    }
+
+    @Test
+    public void createANewGameTest() {
+        PreGameBean preGameBean = new PreGameBean();
+        preGameBean.em = entityManager;
+
+        // Given: a player
+        createPlayer();
+
+        // When: creating a game
+        long gameId = preGameBean.createNewGame(player.getPlayerId());
+
+        // Then: a game is created
+        Assert.assertNotNull(gameId);
     }
 
 
     private void createPlayer() {
         player = new Player();
         player.setName(playerName);
+        player.setEmail(email);
         entityManager.persist(player);
     }
 }
